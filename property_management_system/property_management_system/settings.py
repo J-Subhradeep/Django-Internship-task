20eb58d6733dee4508f9f7d3d6563b5e48d5e7a7
Subhradeep Pal <subhradeeppal30@gmail.com>
2023-01-12 23:27:43 +0530

    keeping security key in environment variable

--- property_management_system/property_management_system/settings.py
+++ property_management_system/property_management_system/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=02jp!sqvdhl=9%cq%ndr_g$lcp+m8fm+xoyt1&)^cy-!0lh0#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
